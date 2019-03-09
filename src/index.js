d834375b00ee2ff58f8230a84a1d9099bc29064d
m1kash <yarik.tobolich@gmail.com>
2019-03-09 05:43:35 +0300

    feat: add functional brackets check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let result;
+
+    (() => {
+        while(str.length) {
+            let notMatchCount = 0;
+            bracketsConfig.forEach(condition => {
+                let config = String(condition[0])+String(condition[1]);
+                if(str.indexOf(config) !== -1) {
+                    str = str.replace(config, '');
+                } else {
+                    notMatchCount++;
+                }
+            });
+            if (bracketsConfig.length === notMatchCount) {
+                if(str.length > 0) {
+                    result = !str.length;
+                }
+                break;
+            }
+        }
+
+        result = !str.length;
+
+    })();
+
+    return result;
+};
